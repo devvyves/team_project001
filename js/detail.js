3d4ff39b770a3886590ad09dd48940227f0012c6
hyeongchan97 <118143118+hyeongchan97@users.noreply.github.com>
2023-01-09 17:52:26 +0900

    detail page  스크롤
    스크롤 이벤트 + 반응형

--- js/detail.js
+++ js/detail.js
@@ -1,4 +1,4 @@
-const Curtain = document.querySelector(".curtain");
+// const Curtain = document.querySelector(".curtain");
 
 /* ========== 커튼 ========== */
 setTimeout(() => {
@@ -27,11 +27,11 @@ function init(){
           slidesPerView:1,
           spaceBetween:0,
         },
-        768:{
+        767:{
           slidesPerView:2,
           spaceBetween:0,
         },
-        1024:{
+        1023:{
           slidesPerView:2,
           spaceBetween:0,
         },
@@ -56,3 +56,74 @@ function init(){
     });
     }
     window.onload = init;
+
+////////////  소개 이벤트 /////////////////
+const elFigure = document.querySelectorAll("div");
+
+window.addEventListener("scroll", function () {
+  elFigure.forEach(function (figu,key) {
+    if (
+      elFigure[key].offsetTop - this.window.innerHeight * 0.8 < window.pageYOffset
+    ) {
+      elFigure[key].classList.add("active");
+    }
+  });
+});
+
+const Curtain = document.querySelector(".curtain");
+
+
+
+
+
+//////////////   스크롤    /////////////////////
+
+
+const elMain = document.querySelector("main"),
+  elAside = document.querySelector("aside"),
+  elSec = document.querySelectorAll("section");
+
+let move = 0;
+
+elSec.forEach(function (elSection, key) {
+  elAside.innerHTML += "<button></button>";
+  elSection.addEventListener("mousewheel", function () {
+    animation(key);
+  });
+});
+
+const elBtns = document.querySelectorAll("aside button");
+let idx = 0;
+elBtns.forEach(function (btn, key) {
+  btn.addEventListener("click", function () {
+    animation(key);
+  });
+});
+
+function update(key) {
+  elBtns[idx].classList.remove("active");
+  elBtns[key].classList.add("active");
+  idx = key;
+}
+
+function animation(key) {
+  try {
+    if (event.wheelDelta < 0) {
+      //마우스휠
+      move = elSec[key].nextElementSibling.offsetTop;
+      key++;
+    } else if (event.wheelDelta > 0) {
+      //마우스휠 
+      move = elSec[key].previousElementSibling.offsetTop;
+      key--;
+    // } else {
+    //   //클릭
+    //   move = elSec[key].offsetTop;
+   }
+  } catch {}
+
+  elMain.style = `transform:translateY(-${move}px)`;
+
+  update(key);
+} 
+
